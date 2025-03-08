908d41d90e0dda280e75f610fd366eefb38f931d
Jarno Luoma-Nirva <jarno.luomanirva@infiniteqsystems.com>
2025-03-08 19:06:15 +0200

    Add helper function to get root directory

--- ex02-express-js/routes/admin.js
+++ ex02-express-js/routes/admin.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     console.log('add-product')
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
