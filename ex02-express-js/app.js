f4b127793f9b99646307bace1bdb16133c065d3d
Jarno Luoma-Nirva <jarno.luomanirva@infiniteqsystems.com>
2025-03-08 16:03:46 +0200

    Parse incoming data

--- ex02-express-js/app.js
+++ ex02-express-js/app.js
@@ -1,11 +1,24 @@
 // 3rd party packages
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     console.log('In the middleware')
     next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    console.log('add-product')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use((req, res, next) => {
