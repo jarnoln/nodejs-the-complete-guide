a43b2b8c8f4843842d2473fcc88238e15206f232
Jarno Luoma-Nirva <jarno.luomanirva@infiniteqsystems.com>
2025-03-08 18:55:37 +0200

    Add 404.html file

--- ex02-express-js/app.js
+++ ex02-express-js/app.js
@@ -1,3 +1,6 @@
+// Core packages
+const path = require('path');
+
 // 3rd party packages
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -13,7 +16,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    // res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
