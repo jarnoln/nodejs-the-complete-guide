94add6de42e24e6ef706f50dc52e9452fa002bcc
Jarno Luoma-Nirva <jarno.luomanirva@infiniteqsystems.com>
2025-03-08 17:58:23 +0200

    Refactor routes to separate files

--- ex02-express-js/app.js
+++ ex02-express-js/app.js
@@ -4,25 +4,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    console.log('In the middleware')
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-    console.log('add-product')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use((req, res, next) => {
-    res.send('<h1>Moro!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
